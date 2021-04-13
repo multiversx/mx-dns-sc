464f53e1df816194a7c7573cc6b9be2bc44e5f31
andrei-marinica <andrei.marinica@elrond.com>
2021-04-14 02:09:02 +0300

    tests use builtin function instead of mock

--- build-wasm.sh
+++ build-wasm.sh
@@ -10,10 +10,3 @@ cd ..
 mkdir -p output
 cp wasm/target/wasm32-unknown-unknown/release/dns_wasm.wasm output/dns.wasm
 cd ..
-
-cd user-mock/wasm
-cargo build --target=wasm32-unknown-unknown --release
-cd ..
-mkdir -p output
-cp wasm/target/wasm32-unknown-unknown/release/user_mock_wasm.wasm output/user-mock.wasm
-cd ..
