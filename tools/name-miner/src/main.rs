ce55a46e024e8f73e711380343dfe326fb387e3d
andrei-marinica <andrei.marinica@elrond.com>
2021-04-14 10:14:54 +0300

    cargo fmt

--- tools/name-miner/src/main.rs
+++ tools/name-miner/src/main.rs
@@ -1,10 +1,10 @@
-use std::env;
 use sha3::{Digest, Keccak256};
+use std::env;
 
 fn keccak256(data: &[u8]) -> [u8; 32] {
-	let mut hasher = Keccak256::new();
-	hasher.update(data);
-	hasher.finalize().into()
+    let mut hasher = Keccak256::new();
+    hasher.update(data);
+    hasher.finalize().into()
 }
 
 /// Lists names that have a certain pattern and that map to a certain shard id.
@@ -15,15 +15,15 @@ fn keccak256(data: &[u8]) -> [u8; 32] {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let prefix = &args[1];
-	let desired_shard_raw = &args[2];
-	let desired_shard = hex::decode(desired_shard_raw).unwrap();
-	assert!(desired_shard.len() == 1, "shard id should be 1 byte");
+    let desired_shard_raw = &args[2];
+    let desired_shard = hex::decode(desired_shard_raw).unwrap();
+    assert!(desired_shard.len() == 1, "shard id should be 1 byte");
 
     for i in 0..5000 {
         let name = format!("{}{:04}.elrond", prefix, i);
-		let hash = keccak256(name.as_bytes());
-		if hash[31] == desired_shard[0] {
-			println!("{}", name);
-		}
+        let hash = keccak256(name.as_bytes());
+        if hash[31] == desired_shard[0] {
+            println!("{}", name);
+        }
     }
 }
