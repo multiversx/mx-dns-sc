04158119b0534aa0e0ab78c2607f25fd9ce46f55
andrei-marinica <andrei.marinica@elrond.com>
2021-05-19 01:59:25 +0300

    build fix, github actions

--- build-wasm.sh
+++ build-wasm.sh
@@ -5,7 +5,7 @@
 export RUSTFLAGS=${RUSTFLAGS-'-C link-arg=-s'}
 
 cd dns/wasm
-cargo build --target=wasm32-unknown-unknown --release
+cargo build --target=wasm32-unknown-unknown --release | return 1
 cd ..
 mkdir -p output
 cp wasm/target/wasm32-unknown-unknown/release/dns_wasm.wasm output/dns.wasm
