a0e48ca411dfb52f288b8732d60e5fedf84a4efe
Andrei Marinica <andrei.marinica@elrond.com>
2022-12-09 11:10:56 +0200

    upgrade to elrond-wasm 0.37.0

--- dns/tests/mandos_test.rs
+++ dns/tests/mandos_test.rs
@@ -4,7 +4,7 @@ fn world() -> BlockchainMock {
     let mut blockchain = BlockchainMock::new();
     blockchain.set_current_dir_from_workspace("dns");
 
-    blockchain.register_contract_builder(
+    blockchain.register_contract(
         "file:output/elrond-wasm-sc-dns.wasm",
         elrond_wasm_sc_dns::ContractBuilder,
     );
